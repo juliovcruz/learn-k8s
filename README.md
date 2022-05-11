d91ee2987e76dfe5c5008b658745cecbbd4215b4
Julio Cruz <juliovcruz0@gmail.com>
2022-05-11 20:12:35 -0300

    chore: add secret

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# How to run
+Install [kind](https://kind.sigs.k8s.io/) and [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/)
 
 1. `kind create cluster --config=k8s/kind.yaml --name=fullcycle`
--- server.go
+++ server.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("ok")))
+	}
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +36,20 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My Family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s Password: %s", user, password)
 }
